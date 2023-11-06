53e30a15beda39aaa5a39d91b11e883d1c1f9add
Andrew Monteith <339531824@GoTVDSB.ca>
2023-11-06 10:39:47 -0500

    done

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Scaling_Using_Rectangles_and_Text
 {
@@ -8,25 +9,37 @@ namespace Scaling_Using_Rectangles_and_Text
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Texture2D blackTexture;
+        Texture2D whiteTexture;
+        SpriteFont titleFont;
+        int xValue, yValue;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            _graphics.PreferredBackBufferHeight = 640;
+            _graphics.PreferredBackBufferWidth = 640;
+            _graphics.ApplyChanges();
+            this.Window.Title = "Chess Board";
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            Random generator = new Random();
+            xValue = generator.Next(_graphics.PreferredBackBufferWidth - 80);
+            yValue = generator.Next(_graphics.PreferredBackBufferHeight - 80);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            blackTexture = Content.Load<Texture2D>("BlackSquare");
+            whiteTexture = Content.Load<Texture2D>("WhiteSquare");
+            titleFont = Content.Load<SpriteFont>("Title");
             // TODO: use this.Content to load your game content here
         }
 
@@ -45,7 +58,19 @@ namespace Scaling_Using_Rectangles_and_Text
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            for (int i = 0; i < _graphics.PreferredBackBufferHeight; i+=_graphics.PreferredBackBufferHeight/4)
+            {
+                for (int j = 0; j < _graphics.PreferredBackBufferWidth; j+=_graphics.PreferredBackBufferWidth/4)
+                {
+                    _spriteBatch.Draw(whiteTexture, new Rectangle (j, i, _graphics.PreferredBackBufferWidth / 8, _graphics.PreferredBackBufferHeight / 8), Color.White);
+                    _spriteBatch.Draw(blackTexture, new Rectangle(j+ _graphics.PreferredBackBufferWidth / 8, i, _graphics.PreferredBackBufferWidth / 8, _graphics.PreferredBackBufferHeight / 8), Color.White);
+                    _spriteBatch.Draw(blackTexture, new Rectangle(j, i + _graphics.PreferredBackBufferHeight / 8, _graphics.PreferredBackBufferWidth / 8, _graphics.PreferredBackBufferHeight / 8), Color.White);
+                    _spriteBatch.Draw(whiteTexture, new Rectangle(j + _graphics.PreferredBackBufferWidth / 8, i + _graphics.PreferredBackBufferHeight / 8, _graphics.PreferredBackBufferWidth / 8, _graphics.PreferredBackBufferHeight / 8), Color.White);
+                }
+            }
+            _spriteBatch.DrawString(titleFont, "Chess", new Vector2 (xValue, yValue), Color.SkyBlue);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
